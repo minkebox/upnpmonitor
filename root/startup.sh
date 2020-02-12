192a94798e4a2a7e4a5851770ac3cd2c453af49c
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-02-11 18:48:46 -0800

    Improve missing UPNP

--- root/startup.sh
+++ root/startup.sh
@@ -1,27 +1,28 @@
 #! /bin/bash
 
-touch /tmp/alltime
+touch /tmp/alltime /tmp/active
 update() {
   raw="$(upnpc -L | grep -E '(UDP|TCP)')"
-  #raw="$(cat tmp/testdata | grep -E '(UDP|TCP)')"
-  echo "$raw" | while read map; do
-    split=($map)
-    #idx=${split[0]}
-    protocol=${split[1]}
-    mapping=${split[2]}
-    fport="${mapping%%->*}"
-    target="${mapping#*->}"
-    ip="${target%%:*}"
-    tport="${target#*:}"
-    tag=$(echo ${split[3]} | sed "s/^'\(.*\)'$/\1/")
-    #blank=${split[4]}
-    #timeout=${split[5]}
-    echo ${fport} [\"${fport}/${protocol}\",\"${ip}\",\"${tport}\",\"${tag}\"]
-  done > /tmp/active
-  cat /tmp/alltime /tmp/active | sort -g | uniq > /tmp/alltime.new
-  cp /tmp/alltime.new /tmp/alltime
-  echo [$(cat /tmp/active | sort -g | sed "s/^[ 0-9]* //" | paste -s -d",")] > /tmp/active.display
-  echo [$(cat /tmp/alltime | sort -g | sed "s/^[ 0-9]* //" | paste -s -d",")] > /tmp/alltime.display
+  if [ "${raw}" != "" ]; then
+    echo "${raw}" | while read map; do
+      split=($map)
+      #idx=${split[0]}
+      protocol=${split[1]}
+      mapping=${split[2]}
+      fport="${mapping%%->*}"
+      target="${mapping#*->}"
+      ip="${target%%:*}"
+      tport="${target#*:}"
+      tag=$(echo ${split[3]} | sed "s/^'\(.*\)'$/\1/")
+      #blank=${split[4]}
+      #timeout=${split[5]}
+      echo ${fport} [\"${fport}/${protocol}\",\"${ip}\",\"${tport}\",\"${tag}\"]
+    done > /tmp/active
+    cat /tmp/alltime /tmp/active | sort -g | uniq > /tmp/alltime.new
+    cp /tmp/alltime.new /tmp/alltime
+    echo [$(cat /tmp/active | sort -g | sed "s/^[ 0-9]* //" | paste -s -d",")] > /tmp/active.display
+    echo [$(cat /tmp/alltime | sort -g | sed "s/^[ 0-9]* //" | paste -s -d",")] > /tmp/alltime.display
+  fi
 }
 
 while true; do
