30629ac60fdde98ce74b13d0d069d090da284300
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-05-02 17:43:07 -0700

    Improve hostname matching

--- root/startup.sh
+++ root/startup.sh
@@ -3,9 +3,8 @@
 touch /tmp/alltime /tmp/active
 update() {
   raw="$(upnpc -L | grep -E '(UDP|TCP)')"
-  if [ "${raw}" = "" ]; then
-    cp /dev/null /tmp/active
-  else
+  cp /dev/null /tmp/active
+  if [ "${raw}" != "" ]; then
     echo "${raw}" | while read map; do
       split=($map)
       #idx=${split[0]}
@@ -14,22 +13,45 @@ update() {
       fport="${mapping%%->*}"
       target="${mapping#*->}"
       ip="${target%%:*}"
-      hostname=$(nslookup ${ip} | grep name | sed "s/^.*name = \(.*\)$/\1/")
-      if [ "${hostname}" = "" ]; then
-        hostname=${ip}
-      fi
+      ##hostname=$(nslookup ${ip} | grep name | sed "s/^.*name = \(.*\)$/\1/")
+      ##if [ "${hostname}" = "" ]; then
+      ##  hostname=${ip}
+      ##fi
       tport="${target#*:}"
       note=$(echo ${split[@]:3:$(expr ${#split[@]}-5)} | sed "s/^'\(.*\)'$/\1/")
       #blank=${split[4]}
       #timeout=${split[5]}
-      echo ${fport} [\"${fport}/${protocol}\",\"${hostname}\",\"${tport}\",\"${note}\"]
-    done > /tmp/active
+      ##echo ${fport} [\"${fport}/${protocol}\",\"${hostname}\",\"${tport}\",\"${note}\"] >> /tmp/active
+      echo ${fport} ${protocol} ${tport} ${ip} ${note} >> /tmp/active
+    done
   fi
   cat /tmp/alltime /tmp/active | sort -g | uniq > /tmp/alltime.new
   cp /tmp/alltime.new /tmp/alltime
   cat /tmp/alltime /tmp/active | sort | uniq -u > /tmp/inactive
-  echo [$(cat /tmp/active | sort -g | sed "s/^[ 0-9]* //" | paste -s -d",")] > /tmp/active.display
-  echo [$(cat /tmp/inactive | sort -g | sed "s/^[ 0-9]* //" | paste -s -d",")] > /tmp/inactive.display
+  echo "$(cat /tmp/active)" | while read map; do
+    if [ "$map" != "" ]; then
+      v=($map)
+      ip=${v[3]}
+      hostname=$(nslookup ${ip} | grep name | sed "s/^.*name = \(.*\)$/\1/")
+      if [ "${hostname}" = "" ]; then
+        hostname=${ip}
+      fi
+      echo ${v[0]} [\"${v[0]}/${v[1]}\",\"${hostname}\",\"${v[2]}\",\"${v[4]}\"]
+    fi
+  done > /tmp/active.entries
+  echo "$(cat /tmp/inactive)" | while read map; do
+    if [ "$map" != "" ]; then
+      v=($map)
+      ip=${v[4]}
+      hostname=$(nslookup ${ip} | grep name | sed "s/^.*name = \(.*\)$/\1/")
+      if [ "${hostname}" = "" ]; then
+        hostname=${ip}
+      fi
+      echo ${v[0]} [\"${v[0]}/${v[1]}\",\"${hostname}\",\"${v[2]}\",\"${v[3]}\"]
+    fi
+  done > /tmp/inactive.entries
+  echo [$(cat /tmp/active.entries | sort -g | sed "s/^[ 0-9]* //" | paste -s -d",")] > /tmp/active.display
+  echo [$(cat /tmp/inactive.entries | sort -g | sed "s/^[ 0-9]* //" | paste -s -d",")] > /tmp/inactive.display
 }
 
 trap "echo 'Terminating'; killall sleep upnpc; exit" TERM
